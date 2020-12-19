f44f2ef2629911f2ccec1647fe01c213be0efbe1
Thomas Butyn <thomasbutyn@pop-os.localdomain>
2020-12-19 13:03:44 -0330

    add movement

--- Game/Main.gd
+++ Game/Main.gd
@@ -6,5 +6,10 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var x_movement = 0.0
+	if Input.is_action_pressed("right"):
+		x_movement += 1.0
+	if Input.is_action_pressed("left"):
+		x_movement -= 1.0
+	$player.position = Vector2($player.position.x + x_movement, $player.position.y)	
