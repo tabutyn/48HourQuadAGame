e8893fc67be48eb272f640a0b5476a15eaa45c32
Jack <jackibandrew1@gmail.com>
2020-12-19 20:07:25 -0400

    death script

--- Game/Player.gd
+++ Game/Player.gd
@@ -4,8 +4,10 @@ extends "res://Main.gd"
 func dead():
 	is_dead = true 
 	current_x_velocity = 0
-	$AnimatedSprite.play("animationname")
+	$AnimatedSprite.play("DeathAnimation")
 	$CollisionShape2d.disabled = true
 	$Timer.start()
 func _on_Timer_timeout() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene("res://Game Over.tscn")
+	if $Player.position.x == 500:
+		dead() 
